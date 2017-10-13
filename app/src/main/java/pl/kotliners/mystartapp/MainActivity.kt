302a866063ef446e26527a0b4a09b0cc44a05072
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-10-13 12:40:36 +0200

    refactor

--- app/src/main/java/pl/kotliners/mystartapp/MainActivity.kt
+++ app/src/main/java/pl/kotliners/mystartapp/MainActivity.kt
@@ -15,23 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-//https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D"dallas%2C%20tx")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
-    fun getSunset(view: View) {
+
+    fun getWeatherCondition(view: View) {
         val city = etCityName.text.toString()
-        val oneWordURL = "https://query.yahooapis.com/v1/public/yql?q=select%20item" +
-                ".condition.text%20from%20weather.forecast%20where%20woeid%20in%20(" +
-                "select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$city%22)" +
-                "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        val oneWordURL = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(" +
+                "select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$city%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
         doAsync {
-            val result = URL(oneWordURL).readText()
+            val result = parseJson(URL(oneWordURL).readText())
             uiThread {
-                parseJson(result)
+                updateTextView(result)
             }
         }
     }
 
-    private fun parseJson(s: String) {
-        val gson = GsonBuilder().create().fromJson(s, Response::class.java)
-        tv.text = "One word weather condition ${gson.query.results.channel.item.condition.text}"
+    private fun parseJson(json: String) = GsonBuilder()
+            .create()
+            .fromJson(json, Response::class.java)
+            .query.results.channel.item.condition.text
+
+    private fun updateTextView(s: String) {
+        tv.text = "One word weather condition $s"
     }
 }
